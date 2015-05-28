8478b4589a99a56182159e9bf409d5d753cd07d5
Nicholas Major <nick@nmajor.com>
2015-05-27 22:59:48 -0600

    Totally redid site with asseble.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,168 @@
+/*
+ * Generated on 2015-05-28
+ * generator-assemble v0.5.0
+ * https://github.com/assemble/generator-assemble
+ *
+ * Copyright (c) 2015 Hariadi Hinta
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// '<%= config.src %>/templates/pages/{,*/}*.hbs'
+// use this if you want to match all subfolders:
+// '<%= config.src %>/templates/pages/**/*.hbs'
+
 module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
+
+    config: {
+      src: 'src',
+      dist: 'dist'
+    },
+
+    watch: {
+      assemble: {
+        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml,scss}'],
+        tasks: ['assemble']
+      },
+      livereload: {
         options: {
-          style: 'compressed',
-          sourcemap: 'file'
+          livereload: '<%= connect.options.livereload %>'
         },
-        files: {
-          'css/main.css' : 'css/main.scss'
+        files: [
+          '<%= config.dist %>/{,*/}*.html',
+          '<%= config.dist %>/assets/{,*/}*.css',
+          '<%= config.dist %>/assets/{,*/}*.js',
+          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      },
+      sass: {
+        files: ['<%= config.src %>/templates/{,*/}*.scss'],
+        tasks: ['sass', 'cssmin']
+      },
+      uglify: {
+        files: ['<%= config.src %>/assets/js/*.js'],
+        tasks: ['uglify']
+      },
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
         }
       }
     },
-    watch: {
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass'],
+
+    assemble: {
+      pages: {
         options: {
-          spawn: false
+          flatten: true,
+          assets: '<%= config.dist %>/assets',
+          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          data: '<%= config.src %>/data/*.{json,yml}',
+          partials: '<%= config.src %>/templates/partials/*.hbs',
+          plugins: ['assemble-contrib-permalinks','assemble-contrib-sitemap'],
+        },
+        files: {
+          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
         }
       }
-    }
+    },
+
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.src %>/templates/assets',
+          src: ['*.scss'],
+          dest: '<%= config.src %>/assets/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.src %>/assets/css',
+          src: ['*.css', '!*.min.css', '!*.css.map'],
+          dest: '<%= config.dist %>/assets/css/',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.src %>/assets/js',
+          src: ['*.js', '!*.min.js'],
+          dest: '<%= config.dist %>/assets/js/',
+          ext: '.min.js'
+        }]
+      }
+    },
+
+    copy: {
+      fonts: {
+        expand: true,
+        cwd: '<%= config.src %>/assets/fonts',
+        src: '**',
+        dest: '<%= config.dist %>/assets/fonts/'
+      },
+      images: {
+        expand: true,
+        cwd: '<%= config.src %>/assets/images',
+        src: '**',
+        dest: '<%= config.dist %>/assets/images/'
+      },
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ['<%= config.dist %>/**/*.{html,xml}']
+
   });
+
+  grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default',['watch']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('server', [
+    'build',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'assemble'
+  ]);
+
+  grunt.registerTask('default', [
+    'build',
+  ]);
+
 };
---
+++ src/assets/js/theme.js
@@ -0,0 +1,13 @@
+$(window).load(function () {
+
+  $("#clickbait-ticker").endlessScroll({
+      width: '100%',
+      height: '20em',
+      steps: -1,
+      speed: 10,
+      mousestop: false
+  });
+
+  // Recopy the previous line to add scrolling to other divs.
+
+});
