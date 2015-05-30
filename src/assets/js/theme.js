1ca2196b9c9beee7907780b891702f9a1cc61b73
Nicholas Major <nick@nmajor.com>
2015-05-29 23:52:16 -0600

    Solid images and layout set

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,12 +1,14 @@
 $(window).load(function () {
 
   $( "#clickbait-ticker" ).rcarousel({
-    auto: { enabled: true, direction: "next", interval: 5000 },
-    visible: 8,
+    auto: { enabled: true, direction: "next", interval: 4000 },
+    visible: 5,
     step: 1,
     height: 300,
     width: 400,
     margin: 50,
   });
+
+  SocialShareKit.init();
 
 });
