92be79adba5acca9df468f2d2147fbdb0ae2bb5e
Nicholas Major <nick@nmajor.com>
2015-06-08 20:41:48 -0600

    Added strike submission form on website

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,6 +1,40 @@
-  var orly;
+// var baseUrl = 'http://api.clickshame.com';
+// var identityKey = 'y1vGmClD5SHK5u9EoiW8TeSpprGUNlQm40UkVnVv2YSvLAJYBx';
 
-$(window).load(function () {
+var identityKey = 'rAHHlL1NY61ul4ncm8eP2DCmKUsbWWzYCgb35pADhdK6KVdtE6';
+var baseUrl = 'http://localhost:3000';
+
+function sendStrike(recaptchaResponse) {
+  $.ajax({
+  url: baseUrl+'/strikes',
+  method: 'POST',
+  data: {
+    key: identityKey,
+    url: $('#report-url').val(),
+    comment: $('#report-comment').val(),
+    type: 'clickbait',
+    recaptcha_response: $('#report-captcha-response').val()
+  },
+  crossDomain: true,
+  }).done(function(result) {
+    console.log('blahsubmittedstrike');
+    console.log(result);
+  });
+}
+
+var captchaContainer = null;
+function loadCaptcha() {
+  console.log('heyhey1');
+  captchaContainer = grecaptcha.render('report-recaptcha', {
+    'sitekey' : '6Ldy9QcTAAAAAIRmro5Uf2I9eFhLrkgzxeZvhTQg',
+    'callback' : function(response) {
+      // $('#report-captcha-response').val(response);
+      sendStrike(response);
+    }
+  });
+}
+
+$(window).load(function() {
 
   $( "#clickbait-ticker" ).rcarousel({
     auto: { enabled: true, direction: "next", interval: 4000 },
@@ -13,11 +47,12 @@ $(window).load(function () {
 
   SocialShareKit.init();
 
-  var baseUrl = 'http://api.clickshame.com';
-  var identityKey = 'y1vGmClD5SHK5u9EoiW8TeSpprGUNlQm40UkVnVv2YSvLAJYBx';
-
-  // var identityKey = 'rAHHlL1NY61ul4ncm8eP2DCmKUsbWWzYCgb35pADhdK6KVdtE6';
-  // var baseUrl = 'http://localhost:3000';
+  function bindReport() {
+    $('#report-submit').click(function() {
+      $('.report-data-form').hide();
+      $('.report-recaptcha-form').show();
+    });
+  }
 
   function getCompositeScore(scores) {
     return scores.filter(function(score) { return score.type === 'composite'; })[0].value;
@@ -101,6 +136,8 @@ $(window).load(function () {
       $('.domain-total').html(result);
     });
   }
+
+  bindReport();
 
   getIdentityTotal();
   getReferenceTotal();
